2cebbc54d39449208b42e0550821024ded15854a
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-11-06 08:45:41 -0300

    feat: login and register pages fully functional

--- src/components/NewUser/index.jsx
+++ src/components/NewUser/index.jsx
@@ -1,30 +1,90 @@
+"use client";
 import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import Link from "next/link";
+import { useState } from "react";
+import axios from "axios";
 
 const NewUser = () => {
+  const [newUser, setNewUser] = useState({
+    username: "",
+    password: "",
+    passwordCheck: ""
+  });
+
+  const onSubmition = async (e) => {
+    e.preventDefault();
+    const {passwordCheck, ...signUp} = newUser;
+    if(passwordCheck != signUp.password) {
+      alert('Passwords must match');
+      return;
+    }
+
+    try {
+      const result = await axios.post("http://localhost:3031/users", signUp);
+      alert("New user created and already logged in!");
+    } catch (error) {
+      alert(error.response.data.message)
+    }
+  };
+
+  const onValueChange = (e) => {
+    const { name, value } = e.target;
+    setNewUser({
+      ...newUser,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="glass-card">
-      <h1 className="text-4xl text-white font-bold text-center mb-6">Register</h1>
-      <form action="submit">
+      <h1 className="text-4xl text-white font-bold text-center mb-6">
+        Register
+      </h1>
+      <form onSubmit={onSubmition}>
         <div className="relative my-4">
-          <input type="text" className="custom-input peer" placeholder=""/>
-          <label htmlFor="" className="moving-label">Username</label>
-          <FontAwesomeIcon icon={faUser} size="1x" className="absolute top-4 right-4"/>
+          <input type="text" name="username" className="custom-input peer" placeholder="" onChange={onValueChange} />
+          <label htmlFor="username" className="moving-label">
+            Username
+          </label>
+          <FontAwesomeIcon
+            icon={faUser}
+            size="1x"
+            className="absolute top-4 right-4"
+          />
         </div>
         <div className="relative my-4">
-          <input type="password" className="custom-input peer" placeholder=""/>
-          <label htmlFor="" className="moving-label">Your Password</label>
-          <FontAwesomeIcon icon={faLock} size="1x" className="absolute top-4 right-4"/>
+          <input type="password" name="password" className="custom-input peer" placeholder="" onChange={onValueChange} />
+          <label htmlFor="password" className="moving-label">
+            Your Password
+          </label>
+          <FontAwesomeIcon
+            icon={faLock}
+            size="1x"
+            className="absolute top-4 right-4"
+          />
         </div>
         <div className="relative my-4">
-          <input type="password" className="custom-input peer" placeholder=""/>
-          <label htmlFor="" className="moving-label">Confirm Password</label>
-          <FontAwesomeIcon icon={faLock} size="1x" className="absolute top-4 right-4"/>
+          <input type="password" name="passwordCheck" className="custom-input peer" placeholder="" onChange={onValueChange} />
+          <label htmlFor="password-check" className="moving-label">
+            Confirm Password
+          </label>
+          <FontAwesomeIcon
+            icon={faLock}
+            size="1x"
+            className="absolute top-4 right-4"
+          />
         </div>
-        <button type="submit" className="primary-button">Sign Up</button>
-        <span>Already an User? <Link href="/" className="text-white/50 font-bold underline">Sign In</Link> </span>
+        <button type="submit" className="primary-button">
+          Sign Up
+        </button>
+        <span>
+          Already an User?{" "}
+          <Link href="/" className="text-white/50 font-bold underline">
+            Sign In
+          </Link>{" "}
+        </span>
       </form>
     </div>
   );
